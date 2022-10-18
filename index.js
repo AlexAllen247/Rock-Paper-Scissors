2fd8973e92ed0a8bf1c69514fee0b506d739f2db
Alex Allen <99975765+AlexAllen247@users.noreply.github.com>
2022-10-18 12:03:33 +0200

    added html and js files, also added play round function and random computer choice function

---
+++ index.js
@@ -0,0 +1,31 @@
+let playerCount = 0
+let coumputerCount = 0
+let winner = ""
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3)
+    switch (randomNumber) {
+        case 0:
+            return "Rock"
+        case 1:
+            return "Paper"
+        case 2:
+            return "Scissors"
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        winner = "Tie"
+    } else if
+    ((playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Paper" && computerSelection === "Rock") || 
+    (playerSelection === "Scissors" && computerSelection === "Paper")) {
+        playerCount++
+        winner = "Player"
+    } else {
+        coumputerCount++
+        winner = "Computer"
+    }
+
+}
